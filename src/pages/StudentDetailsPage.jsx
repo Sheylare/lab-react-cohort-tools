58d909990c523f7db2513b236237617041497c18
Sheyla Arellano <sheylarellanopsico@gmail.com>
2024-07-11 16:43:17 +0200

    lab

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,14 +1,13 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
-
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 function StudentDetailsPage() {
-
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
+  const {studentId} = useParams()
   const studentProfile = studentsData.find((student) => student._id === studentId);
-
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
       <h1>Student Details Page</h1>
@@ -23,7 +22,6 @@ function StudentDetailsPage() {
             <h1 className="text-2xl mt-4 font-bold absolute">
               {studentProfile.firstName} {studentProfile.lastName}
             </h1>
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-24 mb-4 border-b pb-4">
               <p className="text-left mb-2 border-b pb-2">
                 <strong>LinkedIn:</strong>{" "}
@@ -36,27 +34,22 @@ function StudentDetailsPage() {
                   {studentProfile.linkedinUrl}
                 </a>
               </p>
-
               <p className="text-left mb-2 border-b pb-2">
                 <strong>Email:</strong>{" "}
                 <span className="ml-2 text-blue-500 hover:underline">
                   {studentProfile.email}
                 </span>
               </p>
-
               <p className="text-left mb-2 border-b pb-2">
                 <strong>Languages:</strong>{" "}
                 {studentProfile.languages.join(", ")}
               </p>
-
               <p className="text-left mb-2 border-b pb-2">
                 <strong>Program:</strong> {studentProfile.program}
               </p>
-
               <p className="text-left mb-2 pb-2">
                 <strong>Background:</strong> {studentProfile.background}
               </p>
-
               <p className="text-left mb-2 pb-2">
                 <strong>Cohort:</strong>
                 <span className="ml-2 text-blue-500 hover:underline">
@@ -64,18 +57,33 @@ function StudentDetailsPage() {
                 </span>
               </p>
             </div>
-
-
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-              Back
-            </button>
-
+            <Link to = "/"><button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">Back</button></Link>
           </>
         )}
       </div>
     </div>
   );
 }
-
 export default StudentDetailsPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
